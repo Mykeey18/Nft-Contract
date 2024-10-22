378ebf47ebc92091f0c367c87629b525a0a8f8e0
mykeey <mykeey18@gmail.com>
2024-10-21 19:29:07 -0500

    Fix formatting issues

--- test/BasicNftTest.t.sol
+++ test/BasicNftTest.t.sol
@@ -23,7 +23,6 @@ contract BasicNftTest is Test {
         // strings can be compare easily by merely using "=="'to compare the two strings togther
         // We'll leverage abi.encodePacked to convert this to bytes, then finally we can use keccak256 to hash the value into bytes32, which we can can use in our value comparison.
         assert(keccak256(abi.encodePacked(expectedName)) == keccak256(abi.encodePacked(actualName)));
-        
     }
 
     function testUsersCanMintAndHaveBalance() public {
