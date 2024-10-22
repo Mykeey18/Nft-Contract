7f3e4c2396759e57ffb8453cdc5c3b88d1ff2d0a
mykeey <mykeey18@gmail.com>
2024-10-21 19:27:58 -0500

    Update BasicNftTest contract

--- test/BasicNftTest.t.sol
+++ test/BasicNftTest.t.sol
@@ -8,12 +8,29 @@ import {BasicNft} from "src/BasicNft.sol";
 
 contract BasicNftTest is Test {
     DeployBasicNft public deployer;
-    BasicNft public basicnft;
+    BasicNft public basicNft;
+    address public USER = makeAddr("user");
+    string public constant PUG = "ipfs://QmVKvfbUyHrzPUBVQQinQ922sQZ7vszoddM9EA3jPC93xg/?filename=metadata.json";
 
     function setUp() public {
         deployer = new DeployBasicNft();
-        basicnft = deployer.run();
+        basicNft = deployer.run();
     }
 
-    function testNameIsCorrect() public view {}
+    function testNameIsCorrect() public view {
+        string memory expectedName = "Dogie";
+        string memory actualName = basicNft.name();
+        // strings can be compare easily by merely using "=="'to compare the two strings togther
+        // We'll leverage abi.encodePacked to convert this to bytes, then finally we can use keccak256 to hash the value into bytes32, which we can can use in our value comparison.
+        assert(keccak256(abi.encodePacked(expectedName)) == keccak256(abi.encodePacked(actualName)));
+        
+    }
+
+    function testUsersCanMintAndHaveBalance() public {
+        vm.prank(USER);
+        basicNft.mintNft(PUG);
+
+        assert(basicNft.balanceOf(USER) == 1);
+        assert(keccak256(abi.encodePacked(PUG)) == keccak256(abi.encodePacked(basicNft.tokenURI(0))));
+    }
 }
