cd784f853e4a249c507c05d5aa32a26b127cdbdb
mykeey <mykeey18@gmail.com>
2024-10-23 17:10:49 -0500

    Fix Formatting issues

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -17,10 +17,7 @@ contract MoodNft is ERC721 {
 
     mapping(uint256 => Mood) private s_tokenIdToMood;
 
-    constructor(
-        string memory sadSvgImageUri,
-        string memory happySvgImageUri
-    ) ERC721("Mood Nft", "MN") {
+    constructor(string memory sadSvgImageUri, string memory happySvgImageUri) ERC721("Mood Nft", "MN") {
         s_tokenCounter = 0;
         s_sadSvgImageUri = sadSvgImageUri;
         s_happySvgImageUri = happySvgImageUri;
@@ -37,9 +34,7 @@ contract MoodNft is ERC721 {
         return "data:application/json;base64,";
     }
 
-    function tokenURI(
-        uint256 tokenId
-    ) public view override returns (string memory) {
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
         string memory imageURI;
         if (s_tokenIdToMood[tokenId] == Mood.HAPPY) {
             imageURI = s_happySvgImageUri;
@@ -47,28 +42,26 @@ contract MoodNft is ERC721 {
             imageURI = s_sadSvgImageUri;
         }
 
-        return
-            string(
-                abi.encodePacked(
-                    _baseURI(),
-                    Base64.encode(
-                        bytes(
-                            abi.encodePacked(
-                                '{"name": "',
-                                name(),
-                                '", description: "An Nft that reflects your mood!", "attributes": [{"trait_type": "Mood", "value": 100}], "image": ',
-                                imageURI,
-                                '"}'
-                            )
+        return string(
+            abi.encodePacked(
+                _baseURI(),
+                Base64.encode(
+                    bytes(
+                        abi.encodePacked(
+                            '{"name": "',
+                            name(),
+                            '", description: "An Nft that reflects your mood!", "attributes": [{"trait_type": "Mood", "value": 100}], "image": ',
+                            imageURI,
+                            '"}'
                         )
                     )
                 )
-            );
+            )
+        );
         // 1. We created a string out of our JSON object, concatenated with abi.encodePacked.
         // 2. typecast this string as a bytes object
         // 3. encoded the bytes object with Base64
         // 4. concatenated the encoding with our \_baseURI prefix
         // 5. typecast the final value as a string to be returned as our tokenURI
-
     }
 }
