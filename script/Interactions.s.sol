41f4485534a93ad0cf067718cda4e480452b05d7
mykeey <mykeey18@gmail.com>
2024-10-27 17:56:28 -0500

    Update Interaction file

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -5,19 +5,33 @@ pragma solidity ^0.8.18;
 import {Script} from "forge-std/Script.sol";
 import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 import {BasicNft} from "src/BasicNft.sol";
+import {MoodNft} from "src/MoodNft.sol";
 
 contract MintBasicNft is Script {
     string public constant PUG = "ipfs://QmVKvfbUyHrzPUBVQQinQ922sQZ7vszoddM9EA3jPC93xg/?filename=metadata.json";
 
     function run() external {
-        address contactAddress = 0x288B1b4B0C9733675f959254426C1dE96208a235;
+        address mostRecentlyDeployedBasicNft = DevOpsTools.get_most_recent_deployment("BasicNft", block.chainid);
 
-        mintNftOnContract(contactAddress);
+        mintNftOnContract(mostRecentlyDeployedBasicNft);
     }
 
-    function mintNftOnContract(address contractAddress) public {
+    function mintNftOnContract(address basisNftAddress) public {
         vm.startBroadcast();
-        BasicNft(contractAddress).mintNft(PUG);
+        BasicNft(basisNftAddress).mintNft(PUG);
+        vm.stopBroadcast();
+    }
+}
+
+contract MintMoodNft is Script {
+    function run() external {
+        address mostRecentlyDeployedMoodNft = DevOpsTools.get_most_recent_deployment("MoodNft", block.chainid);
+        mintNftMood(mostRecentlyDeployedMoodNft);
+    }
+
+    function mintNftMood(address MoodNftAddress) public {
+        vm.startBroadcast();
+
         vm.stopBroadcast();
     }
 }
